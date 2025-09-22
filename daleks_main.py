16b20d1988238a5849a5c2eefb21d90b45e1a0a5
Zachary Lacasse <zachary_lacasse@outlook.com>
2025-09-22 14:17:34 -0400

    Ajout UI & Death Screen

--- daleks_main.py
+++ daleks_main.py
@@ -2,20 +2,32 @@
 from msvcrt import getch
 import os
 from msvcrt import getch
+import random
+from time import sleep
 
 # Codes de couleur pour console
-GREEN = '\033[32m'
-WHITE = '\033[0m'
+RED = "\033[31m"
+GREEN = "\033[32m"
+YELLOW = "\033[1;33m"
+BROWN = "\033[0;33m"
+CYAN = "\033[0;36m"
+WHITE = "\033[0m"
+
+# Variables 
+MAX_DALEKS = 3
+Score = 0
+daleks = []
 
 class Grille :
+    # longueur et largeur MAX de la grille 
     TAS_X, TAS_Y = 10, 10
 
-    VIDE = "O"
-    ZAP = "Z"
-    FERRAILE = "X"
-    TELEPORTEUR = "T"
-    DALEK = "D"
-    JOUEUR = f"{GREEN}J{WHITE}"
+    VIDE = f"{WHITE}O"
+    ZAP = f"{CYAN}Z"
+    FERRAILE = f"{BROWN}X"
+    TELEPORTEUR = f"{YELLOW}T"
+    DALEK = f"{RED}D"
+    JOUEUR = f"{GREEN}J"
 
     grille = [
     [VIDE, VIDE, VIDE, VIDE, VIDE, VIDE, VIDE, VIDE, VIDE, VIDE],
@@ -30,19 +42,12 @@ class Grille :
     [VIDE, VIDE, VIDE, VIDE, VIDE, VIDE, VIDE, VIDE, VIDE, VIDE], 
     ]
 
-def affichage() :
-      for y in range (0, Grille.TAS_Y) :
-        for x in range (0, Grille.TAS_X):
-            print(Grille.grille[y][x] + ' ', end = '')
-        print('')
-
 # Le joueur
 class Player :
     # Constructeur
-    def __init__(self, posX, posY, vies, teleporteur, zappeur):
+    def __init__(self, posX, posY, teleporteur, zappeur):
         self.posX = posX
         self.posY = posY
-        self.vies = vies
         self.teleporteur = teleporteur
         self.zappeur = zappeur
 
@@ -67,44 +72,112 @@ class Player :
     def getPos(self):
         return (self.posX, self.posY)
 
+# Dalek
+class Dalek :
+    # Constructeur
+    def __init__(self, posX, posY):
+        self.posX = posX
+        self.posY = posY
+        self.etat = True
+
+    # Chasse le joueur
+    def chasse(self, docteur):
+        docX = docteur.posX
+        docY = docteur.posY
+
+        # Y et X sont inversés, position précédente remise a vide.
+        Grille.grille[self.posY][self.posX] = Grille.VIDE
+
+        if (self.posY < docY):
+            self.posY += 1
+        elif (self.posY > docY):
+            self.posY -= 1
+        elif (self.posX < docX):
+            self.posX += 1
+        elif (self.posX > docX):
+            self.posX -= 1
+
+# affichage de la grille (y = rangée, x = colonne)
+def grilleAffichage() :
+      for y in range (0, Grille.TAS_Y) :
+        for x in range (0, Grille.TAS_X):
+            print(Grille.grille[y][x] + ' ', end = '')
+        print('')
+
+# affichage du UI
+def uiAffichage():
+    print("\n UTILISER W,A,S,D POUR SE DÉPLACER    |    Q = Zappeur    |    E = Téléportation    |    O = Arrêter de jouer")
+    print("\n\n")
+    print(f"{RED}Daleks restants : " + str(daleks.count))
+    print(f"{CYAN}Téléporteurs utilisables : " + str(docteur.teleporteur))
+    print(f"{YELLOW}Zappeurs utilisables : " + str(docteur.zappeur))
+    print(f"{WHITE}Score : " + str(Score))
 
 # Menu principal
 game_over = True
+jouer = True
+while jouer :
+    print("Jeu du dalek! \n\n\n")
+    print("Voulez vous Jouer ? (o/n)")
+    choix_de_jouer = getch()
 
-print("Jeu du dalek! \n\n\n")
-print("Voulez vous Jouer ? (o/n)")
-choix_de_jouer = getch()
+    if (choix_de_jouer == b'o'):
+        game_over = False
+        _ = os.system('cls')
+    elif (choix_de_jouer == b'n'):
+        jouer = False
 
-if (choix_de_jouer == b'o'):
-    game_over = False
-    _ = os.system('cls')
-    
+    # Création d'un objet joueur nommé docteur
+    docteur = Player(random.randint(1, 9), random.randint(1, 9), 0, 0)
 
-# Création d'un objet joueur nommé docteur
-docteur = Player(0, 0, 4, 0, 0)
+    # création des daleks
+    for i in range(MAX_DALEKS):
+        # vérifie si la position est prise
+        while True:
+            posX = random.randint(1, 9)
+            posY = random.randint(1, 9)
+            position_prise = False
+            # vérifie chaque element de la liste daleks, si la position est prise, recommence le procès
+            for d in daleks:
+                if (d.posX == posX and d.posY == posY):
+                    position_prise = True
+                    break
+            # position n'est pas prise!
+            if (position_prise == False):
+                daleks.append(Dalek(posX, posY))
+                break
+            
+    # Déroulement du jeu
+    while not game_over:
+        _ = os.system('cls')
+        
+        # Affichage position Joueur et Daleks
+        Grille.grille[docteur.posY][docteur.posX] = Grille.JOUEUR
 
+        for i in range(MAX_DALEKS):
+            Grille.grille[daleks[i].posY][daleks[i].posX] = Grille.DALEK
 
-# Déroulement du jeu
-while not game_over:
-    _ = os.system('cls')
-    
-    # Affichage position Joueur
-    Grille.grille[docteur.posY][docteur.posX] = Grille.JOUEUR
+        grilleAffichage()
+        uiAffichage()
 
-    affichage()
+        playerInput = getch()    
 
-    playerInput = getch()    
-
-    if (playerInput == b'w'):
-        docteur.move(0,1)
-    elif (playerInput == b's'):
-        docteur.move(0,-1)
-    elif (playerInput == b'a'):
-        docteur.move(-1,0)
-    elif (playerInput == b'd'):
-        docteur.move(1,0)
-    elif (playerInput == b'o'):
-        game_over = True
-    
-
-
+        if (playerInput == b'w'):
+            docteur.move(0,1)
+        elif (playerInput == b's'):
+            docteur.move(0,-1)
+        elif (playerInput == b'a'):
+            docteur.move(-1,0)
+        elif (playerInput == b'd'):
+            docteur.move(1,0)
+        elif (playerInput == b'o'):
+            game_over = True
+        
+        for i in range(MAX_DALEKS):
+            daleks[i].chasse(docteur)
+            if (daleks[i].posX == docteur.posX and daleks[i].posY == docteur.posY) :
+                _ = os.system('cls')
+                print(f"{RED}GAME OVER !{WHITE}")
+                sleep(2)
+                _ = os.system('cls')
+                game_over = True
